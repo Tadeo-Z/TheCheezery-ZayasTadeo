ac128b55b47d11681801296b3d9ef657adfcb01b
Tadeo-Z <fernando.zayas228310@potros.itson.edu.mx>
2024-06-10 22:20:20 -0700

    Añadidos listViews

---
+++ app/src/main/java/zayas/tadeo/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package zayas.tadeo.thecheezery
+
+data class Product(var name: String,
+                   var image: Int,
+                   var descripcion: String,
+                   var price: Double)
